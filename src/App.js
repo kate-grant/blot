75843d934be2c2548e1c40ad533cd4cb3fb4c635
Kate <hello.kategrant@gmail.com>
2022-04-16 12:45:15 -0400

    add useRef and useEffect to implement better accessibility with focus flow

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 import { nanoid } from "nanoid";
 
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
-  console.log(0.5, props);
-  console.log(1, props?.tasks);
+  const listHeadingRef = useRef(null);
 
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
 
   console.log(1, tasks);
 
@@ -42,7 +58,7 @@ function App(props) {
     setTasks(editedTaskList);
   }
 
-  const taskList = tasks.map(task => (
+  const taskList = tasks.filter(FILTER_MAP[filter]).map(task => (
     <Todo
         id={task.id}
         name={task.name}
@@ -55,19 +71,32 @@ function App(props) {
     )
   );
   
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton 
+    key={name}
+    name={name}
+    isPressed={name === filter}
+    setFilter={setFilter}
+    />
+  ));
+
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
   
-
+  const prevTaskLength = usePrevious(tasks.length);
+  
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
   
   return (
     <div className="todoapp stack-large">
       <h1>Blot</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <ul
         role="list"
@@ -76,7 +105,7 @@ function App(props) {
       >
         {taskList}
       </ul>
-      <h2 id="list-heading">{headingText}</h2>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>{headingText}</h2>
       
     </div>
   );
